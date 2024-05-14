c2374ec6ea5beab52b65da9fc998e0f940afc384
Tausif-Ansari-Oficial <160697681+Tausif-Ansari-Oficial@users.noreply.github.com>
2024-05-14 19:51:37 +0530

    Update SmackDown.jsx

--- src/Project/Game/SmackDown.jsx
+++ src/Project/Game/SmackDown.jsx
@@ -3,6 +3,7 @@ import SD from "./Image/sd.png";
 
 export default function SmackDown() {
   return (
+{/* This is only image container (nothig more) */}
     <img src={SD} alt="sm" className='logo'/>
   )
 }
