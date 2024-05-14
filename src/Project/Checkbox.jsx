9dc90f46418a19dcfe0af12e9212f7bba0083bb1
Tausif-Ansari-Oficial <160697681+Tausif-Ansari-Oficial@users.noreply.github.com>
2024-05-14 20:24:35 +0530

    Update Checkbox.jsx

--- src/Project/Checkbox.jsx
+++ src/Project/Checkbox.jsx
@@ -1,20 +1,34 @@
+// to use any context in to current components use"useContext()" hook
 import React, { useContext } from 'react';
+
+// import Acontex from maain component 
 import { Acontex } from './Main';
 
 export default function Checkbox() {
+  
+// stor imported cotext to a useContext
   const { state, setState } = useContext(Acontex);
 
+// create a changeHandle function to handle each input changes
   const changeHandle = (e) => {
-    
+
+// Extract the name and checked properties from the event's target (the input elements)
     const { name, checked } = e.target;
+
+// Update the state using setState
+  // Return a new state object that contains all properties of the state  and updates the property accroding to the input's name with its checked value
     setState((argu) => ({ ...argu, [name]: checked }));
+
+// check state to console
     console.log(state);
   };
 
   return (
     <div>
+{/* a normal form tag */}
       <form action="" method="post">
-        
+
+{/* bgmi list */}
       <div className="form-check">
           <input
             className='form-check-input'
@@ -24,7 +38,7 @@ export default function Checkbox() {
             checked={state.BGMI}
           />
           <label htmlFor="BGMI">BGMI</label>
-        </div>
+      </div>
       
       <div className="form-check">
           <input
