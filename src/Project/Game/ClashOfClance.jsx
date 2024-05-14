56b1a16c241c63549bfe36e5ec65df4cba127aac
Tausif-Ansari-Oficial <160697681+Tausif-Ansari-Oficial@users.noreply.github.com>
2024-05-14 19:50:48 +0530

    Update ClashOfClance.jsx

--- src/Project/Game/ClashOfClance.jsx
+++ src/Project/Game/ClashOfClance.jsx
@@ -4,6 +4,7 @@ import '../../Project/style.css'
 
 export default function ClashOfClance() {
   return (
+{/* This is only image container (nothig more) */}
     <img src={COC} alt="coc" className='logo'/>
   )
 }
