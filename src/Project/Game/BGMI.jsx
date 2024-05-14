e76e522cc69588e5d8fc3d0ca2372187157318fb
Tausif-Ansari-Oficial <160697681+Tausif-Ansari-Oficial@users.noreply.github.com>
2024-05-14 19:50:19 +0530

    Update BGMI.jsx

--- src/Project/Game/BGMI.jsx
+++ src/Project/Game/BGMI.jsx
@@ -5,6 +5,7 @@ import '../../Project/style.css'
 export default function BGMI() {
   return (
     <div>
+{/* This is only image container (nothig more) */}
       <img src={Bgmi} alt="bgmi.png" className='logo'/>
     </div>
   );
